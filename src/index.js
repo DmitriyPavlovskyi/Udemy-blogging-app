c823ec91ea34671684a23e4d547fa9a7c7992e26
dmitriypavlovskyi <pavlovskyidmitriy@gmail.com>
2018-03-26 13:38:28 +0300

    Init commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './root/reducer';
+
+import Root from './root/app';
+
+const createStoreWithMiddleware = applyMiddleware(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Root />
+  </Provider>
+  , document.querySelector('.container'));
