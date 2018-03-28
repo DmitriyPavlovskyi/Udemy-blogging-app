a4802e85aca52d696e26dc3c65dee84be846fdc3
dmitriypavlovskyi <pavlovskyidmitriy@gmail.com>
2018-03-28 12:34:04 +0300

    Basic validation added

--- src/features/newPosts/index.js
+++ src/features/newPosts/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../shared/AC/posts';
+import { Link } from 'react-router';
+import './styles.css';
 
 class PostsNew extends Component {
   render() {
@@ -9,25 +11,54 @@ class PostsNew extends Component {
     return (
       <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create A New Post</h3>
+
         <div className="form-group">
           <label>Title</label>
           <input type="text" className="form-control" {...title} />
+          <div className="text-help text-danger">
+            {title.touched ? title.error : ''}
+          </div>
         </div>
 
         <div className="form-group">
           <label>Categories</label>
           <input type="text" className="form-control" {...categories} />
+          <div className="text-help text-danger">
+            {categories.touched ? categories.error : ''}
+          </div>
         </div>
 
         <div className="form-group">
           <label>Content</label>
           <textarea className="form-control" {...content} />
+          <div className="text-help text-danger">
+            {content.touched ? content.error : ''}
+          </div>
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
+}
+
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Enter a username';
+  }
+
+  if (!values.categories) {
+    errors.categories = 'Enter categories';
+  }
+
+  if (!values.content) {
+    errors.content = 'Enter some content';
+  }
+
+  return errors;
 }
 
 // connect: 1st argument is mapStateToProps, 2nd is mapDispatchToProps
@@ -36,6 +67,7 @@ class PostsNew extends Component {
 export default reduxForm({
   form: 'NewPostsForm',
   fields: ['title', 'categories', 'content'],
+  validate
 }, null, { createPost })(PostsNew);
 
 // user types comething in .... record it on app state
